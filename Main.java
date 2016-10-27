4cb772b2cadf1ac65ad6e9af5847a14bcff8e8e0
Iceiceiceiceice <david_b987@hotmail.com>
2016-10-27 16:14:04 -0400

    fixes #32

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
-public static main void (String[] args) {
-  //do something
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
